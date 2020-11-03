efca62234c07d0f703eb0fb65f060da6befb5e00
Michal <michalogloza97@gmail.com>
2020-11-03 15:06:26 +0100

    added StringInfo data structure

---
+++ StringInfo.py
@@ -0,0 +1,7 @@
+class StringInfo:
+
+    def __init__(self, upper_count, lower_count, digit_count, special_count):
+        self.upper_count = upper_count
+        self.lower_count = lower_count
+        self.digit_count = digit_count
+        self.special_count = special_count
