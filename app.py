e82a5a736b88af60228340c620dd4519117bbdfc
Michal <michalogloza97@gmail.com>
2020-11-03 15:02:08 +0100

    updated last endpoint to return all required data

--- app.py
+++ app.py
@@ -11,7 +11,11 @@ def home():
 @app.route("/count/<text>")
 def count(text):
     upper_count = sum(1 for c in text if c.isupper())
-    return "upper_count: {}".format(upper_count)
+    lower_count = sum(1 for c in text if c.islower())
+    digit_count = sum(1 for c in text if c.isdigit())
+    special_count = sum(1 for c in text if not (c.isalpha() or c.isdigit()))
+    return "upper_count: {0} <br/> lower_count: {1} <br/> digit_count: {2} <br/> special_count: {3}"\
+        .format(upper_count, lower_count, digit_count, special_count)
 
 
 if __name__ == '__main__':
