a8499f3fbf96d4437f0319070d6645299c28f020
Michal <michalogloza97@gmail.com>
2020-11-03 14:58:55 +0100

    created endpoint which returns number of uppercase characters

--- app.py
+++ app.py
@@ -8,5 +8,11 @@ def home():
     return "This API counts characters of various types in string. Available at address: /count/[textArgument]"
 
 
+@app.route("/count/<text>")
+def count(text):
+    upper_count = sum(1 for c in text if c.isupper())
+    return "upper_count: {}".format(upper_count)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
